c281087934ee3c93bf42df91369848ad85d94e6c
Aderito <aderito.muniunga@kinsari.com>
2025-11-13 16:44:47 +0100

    #Aula 10 --- Repository--- Nesta aula, aprendemos como o Spring pode gerenciar o ciclo de vida de um repositório. Utilizando a anotação @Autowired, podemos pedir ao Spring para instanciar e gerenciar nosso repositório, garantindo que ele seja inicializado corretamente. Com isso, podemos acessar os métodos disponíveis no JPA Repository, como o método save, para salvar entidades no banco de dados. Também vimos como criar uma interface de repositório, estendendo a classe JpaRepository do Spring Data JPA, e definindo o tipo da entidade e do ID. Na próxima aula, iremos validar a criação de usuários duplicados.

--- src/main/java/com/muniunga/todolist/user/UserController.java
+++ src/main/java/com/muniunga/todolist/user/UserController.java
@@ -3,6 +3,7 @@ package com.muniunga.todolist.user;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -10,11 +11,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RequestMapping("/users")
 public class UserController {
    
+    @Autowired
+    private IUserRepository userRepository;
     @PostMapping
-    public void createUser(@RequestBody UserModel userModel) {
-        System.out.println("Username: " + userModel.getUsername());
-        System.out.println("Name: " + userModel.getName());
-        System.out.println("Password: " + userModel.getPassword());
+    public UserModel createUser(@RequestBody UserModel userModel) {
+       var savedUser = this.userRepository.save(userModel);
+       return savedUser;
     }    
 
 }

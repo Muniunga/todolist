62cc939dbf78d4f4f7c7c1ceaa2b655a79eb9f97
Aderito <aderito.muniunga@kinsari.com>
2025-11-13 16:33:45 +0100

    Nesta aula do curso introdutorio de Java com SpringBoot aprendi como armazenar informações de usuários em um banco de dados usando o Spring Data JPA. O Spring Data JPA é um projeto do Spring que nos permite fazer a comunicação com o banco de dados e manipular os dados de forma fácil. Ele utiliza o conceito de ORM (Mapeamento Objeto-Relacional) para transformar objetos em uma forma que o banco de dados entenda. Vamos utilizar o H2 como banco de dados em memória para fins de aprendizado, mas em produção, você pode usar outros bancos de dados como PostgreSQL ou MySQL. Vamos configurar as dependências no arquivo pom.xml e também configurar o H2 no arquivo application.properties. Depois de configurado, podemos acessar o console do H2 para verificar se está tudo funcionando corretamente. Nas próximas aulas, vamos construir as tabelas e entender como o Spring Data JPA se comunica com o banco de dados. criar a tabela de usuários em nossa aplicação usando o conceito de ORM (Object-Relational Mapping). Definimos a entidade "User" como nossa tabela de usuários no banco de dados. Também adicionamos a chave primária, que é um ID único gerado automaticamente usando o tipo UUID. Além disso, definimos outros atributos como colunas na tabela, como "Username", "Password", "Name" e "CreatedAt". Observamos que o banco de dados utiliza o padrão de separação de palavras com underline.

--- src/main/java/com/muniunga/todolist/user/UserModel.java
+++ src/main/java/com/muniunga/todolist/user/UserModel.java
@@ -1,12 +1,30 @@
 package com.muniunga.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
     
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+//  Entende que os nomes das colunas no banco de dados serão os que estão abaixo mas caso eu queira mudar posso usar @Column(name = "nome_da_coluna")
     private String username;
     private String name;
     private String password;
+
+    // Anotacão para definir que esse campo será preenchido automaticamente com a data e hora de criação do registro
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }

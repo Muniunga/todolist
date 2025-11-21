5183f0b1335177a3eba6dfefcb1416ed4995a967
Aderito <aderito.muniunga@kinsari.com>
2025-11-21 10:52:22 +0100

    #Aula 12 --- Utilizando ResponseEntity--- Nesta aula aprendi como ter retornos diferentes na nossa aplicação dentro da mesma requisição. Utilizaremos o tipo Response Entity do Spring Framework HTTP para retornar mensagens de erro e status code adequados. Podemos utilizar o status code 400 para indicar erros, como quando o usuário já existe. Também podemos utilizar o status code 201 para indicar sucesso, como quando um usuário é criado. O Response Entity nos permite retornar tanto casos de sucesso como casos de erro de forma eficiente.

--- src/main/java/com/muniunga/todolist/user/UserController.java
+++ src/main/java/com/muniunga/todolist/user/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
@@ -14,15 +16,16 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping
-    public UserModel createUser(@RequestBody UserModel userModel) {
+    public ResponseEntity createUser(@RequestBody UserModel userModel) {
        var user = this.userRepository.findByUsername(userModel.getUsername());
          if (user != null) {
-            System.out.println("Username already exists");
-            //  throw new RuntimeException("Username already exists");
-            return null;
+            System.out.println("Username already exists");        
+            // Mensagem de erro
+            // status code 409
+            return ResponseEntity.status(HttpStatusCode.valueOf(409)).body("Username already exists");
          }
         var savedUser = this.userRepository.save(userModel);
-        return savedUser;
+        return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(savedUser);
     }
 
 }
